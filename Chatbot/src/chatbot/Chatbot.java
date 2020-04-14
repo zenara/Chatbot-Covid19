04d3e89333cbd10ea7bdf03e4ac137fe8954718a
Chathurika Senani <zenaronline@gmail.com>
2020-04-14 15:35:47 +0530

    Chat bot - 'covidiot' implemented

--- Chatbot/src/chatbot/Chatbot.java
+++ Chatbot/src/chatbot/Chatbot.java
@@ -5,17 +5,71 @@
  */
 package chatbot;
 
+import java.io.File;
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.History;
+import org.alicebot.ab.MagicBooleans;
+import org.alicebot.ab.MagicStrings;
+import org.alicebot.ab.utils.IOUtils;
+
 /**
  *
  * @author Senani
  */
 public class Chatbot {
+    
+    private static final boolean TRACE_MODE = false;
+    static String botName = "covidiot";
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+ 
+            String resourcesPath = getResourcesPath();
+            System.out.println(resourcesPath);
+            MagicBooleans.trace_mode = TRACE_MODE;
+            Bot bot = new Bot("covidiot", resourcesPath);
+            Chat chatSession = new Chat(bot);
+            bot.brain.nodeStats();
+            String textLine = "";
+ 
+            while(true) {
+                System.out.print("Human : ");
+                textLine = IOUtils.readInputTextLine();
+                if ((textLine == null) || (textLine.length() < 1))
+                    textLine = MagicStrings.null_input;
+                if (textLine.equals("q")) {
+                    System.exit(0);
+                } else if (textLine.equals("wq")) {
+                    bot.writeQuit();
+                    System.exit(0);
+                } else {
+                    String request = textLine;
+                    if (MagicBooleans.trace_mode)
+                        System.out.println("STATE=" + request + ":THAT=" + ((History) chatSession.thatHistory.get(0)).get(0) + ":TOPIC=" + chatSession.predicates.get("topic"));
+                    String response = chatSession.multisentenceRespond(request);
+                    while (response.contains("&lt;"))
+                        response = response.replace("&lt;", "<");
+                    while (response.contains("&gt;"))
+                        response = response.replace("&gt;", ">");
+                    System.out.println("Covidiot : " + response);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    private static String getResourcesPath() {
+        File currDir = new File(".");
+        String path = currDir.getAbsolutePath();
+        path = path.substring(0, path.length() - 2);
+        System.out.println(path);
+        String resourcesPath = path + File.separator + "src" + File.separator + "resources";
+        return resourcesPath;
     }
     
 }
